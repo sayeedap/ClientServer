d2e9cf3dfdf3ba711db21f19e0ae3c8419f4070e
sruthi <sruthirajan444@gmail.com>
2018-03-27 14:36:23 +0530

    File modified

--- Client.java
+++ Client.java
@@ -6,3 +6,8 @@ public static void main(String args[])throws Exception{
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+    String str="",str2="";  
+    while(!str.equals("stop")){  
+    str=br.readLine();  
+    dout.writeUTF(str);  
+
