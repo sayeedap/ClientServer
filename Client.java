d586971f686d484876194ef0541b57930266bbd2
sruthi <sruthirajan444@gmail.com>
2018-03-27 14:31:47 +0530

    File modified

--- Client.java
+++ Client.java
@@ -1,3 +1,8 @@
  import java.net.*;  
     import java.io.*;  
-    class Client{  
+    class MyClient{  
+public static void main(String args[])throws Exception{  
+    Socket s=new Socket("localhost",3333);  
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
