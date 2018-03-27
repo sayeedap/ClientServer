16d25a5a73dc88dfb2c21a0296728b38af2f3eeb
Sumisha <sumishasunil98@gmail.com>
2018-03-27 14:54:52 +0530

    Server Update

--- Server.java
+++ Server.java
@@ -1,6 +1,6 @@
 import java.net.*;  
     import java.io.*; 
- class MyServer{  
+ class Server{  
     public static void main(String args[])throws Exception{  
     ServerSocket ss=new ServerSocket(3333); 
 Socket s=ss.accept();  
