1a65a7466ed310fc414aa4fd219c4d0f4163a145
Sumisha <sumishasunil98@gmail.com>
2018-03-27 14:43:28 +0530

    Server Update

--- Server.java
+++ Server.java
@@ -3,4 +3,21 @@ import java.net.*;
  class MyServer{  
     public static void main(String args[])throws Exception{  
     ServerSocket ss=new ServerSocket(3333); 
+Socket s=ss.accept();  
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+    System.out.println("Type stop To Close the communication ");
+    String str="",str2="";  
+    while(!str.equals("stop")){  
+    str=din.readUTF();  
+    System.out.println("Client Says: "+str);  
+    str2=br.readLine();  
+    dout.writeUTF(str2);  
+    dout.flush();  
+    }  
+    din.close();  
+    s.close();  
+    ss.close();  
+    }}  
 
